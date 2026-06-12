8f321a798ef8afa82565c86c96859d4d9f081ec0
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2026-06-12 12:04:20 -0300

    feat: Añadir modelos de solicitud de inicio de sesión y registro

---
+++ frontend/src/app/shared/models/auth.model.ts
@@ -0,0 +1,16 @@
+export interface LoginRequest {
+  username: string;
+  password: string;
+  rememberMe?: boolean;
+}
+
+export interface JwtToken {
+  id_token: string;
+}
+
+export interface RegisterRequest {
+  login: string;
+  email: string;
+  password: string;
+  langKey: string;
+}
