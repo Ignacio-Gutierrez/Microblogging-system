527a13baf8b1e8e8518b9882574e5e5db9b7db24
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2026-06-22 19:02:44 -0300

    feat: Actualizar pruebas de creación de blogs para usar identificadores únicos

--- cypress_e2e/cypress/e2e/blog/create.cy.js
+++ cypress_e2e/cypress/e2e/blog/create.cy.js
@@ -1,4 +1,6 @@
 describe('Blog', () => {
+  const uniqueId = Date.now();
+
   beforeEach(() => {
     cy.visit('http://localhost:8100/login');
     cy.get('[data-cy="login-username-input"]').type('user');
@@ -12,12 +14,12 @@ describe('Blog', () => {
 
     cy.get('[data-cy="create-blog-button"]').click();
 
-    cy.get('[data-cy="blog-name-input"]').type('Mi Blog de Prueba');
-    cy.get('[data-cy="blog-handle-input"]').type('mi-blog-prueba');
+    cy.get('[data-cy="blog-name-input"]').type(`Blog ${uniqueId}`);
+    cy.get('[data-cy="blog-handle-input"]').type(`blog-${uniqueId}`);
 
     cy.get('[data-cy="blog-modal-submit-button"]').click();
 
     cy.get('[data-cy="blog-modal-submit-button"]', { timeout: 10000 }).should('not.exist');
-    cy.contains('Mi Blog de Prueba').should('be.visible');
+    cy.contains(`Blog ${uniqueId}`).should('be.visible');
   });
 });
