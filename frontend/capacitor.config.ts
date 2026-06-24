5fd1a699d4a9dd44a6d1f9c72ee2e014de6f01cf
Ignacio-Gutierrez <ij.gutierrez@alumno.um.edu.ar>
2026-06-24 10:50:38 -0300

    feat: Actualizar configuración de Capacitor y Nginx, y añade ID al manifest

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'frontend',
+  appId: 'com.microblogging.app',
+  appName: 'Microblogging',
   webDir: 'www'
 };
 
